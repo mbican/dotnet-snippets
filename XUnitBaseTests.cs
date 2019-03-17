7dfc83734d0c2f4ae645075e4f038d9bb6c85c38
Michal Bičan <bicanm@gmail.com>
2019-03-17 10:38:25 +0000

    Add XUnitBaseTests

---
+++ XUnitBaseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit.Abstractions;
+
+namespace Mbican.DotnetSnippets
+{
+    public abstract class XUnitBaseTests
+    {
+        protected readonly ITestOutputHelper TestOutput;
+        private readonly Lazy<ITest> TestLazy;
+        protected ITest Test => TestLazy.Value;
+        private readonly Lazy<string> TestIdLazy;
+        protected string TestId => TestIdLazy.Value;
+
+        public XUnitBaseTests(ITestOutputHelper testOutputHelper)
+        {
+            TestOutput = testOutputHelper;
+            TestLazy = new Lazy<ITest>(() => TestOutput.GetTest());
+            TestIdLazy = new Lazy<string>(() => Test.TestCase.TestMethod.TestClass.Class.Name
+                + "/" + Test.TestCase.TestMethod.Method.Name
+                // for [Theory] tests there are multiple tests per method they differ in UniqueId
+                + "/" + Test.TestCase.UniqueID);
+        }
+    }
+}
